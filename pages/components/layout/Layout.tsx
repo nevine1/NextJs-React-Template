58ede813914cd55d506e6ecb8e3ce59c71832711
nevine1 <venasamaan@gmail.com>
2022-09-26 18:36:17 -0400

    saving new files

---
+++ pages/components/layout/Layout.tsx
@@ -0,0 +1,13 @@
+import React, { ReactElement, Fragment } from 'react'
+
+interface Props {
+    
+}
+
+export default function Layout({}: Props): ReactElement {
+    return (
+        <Fragment>
+            <h1>hello layout</h1>
+        </Fragment>
+    )
+}
