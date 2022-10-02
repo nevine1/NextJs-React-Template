6f4037f32c28c6bb849742ec0f49ce4c7ac6e94c
nevine1 <venasamaan@gmail.com>
2022-10-01 21:39:07 -0400

    saved index.tsx

---
+++ components/overView/index.tsx
@@ -0,0 +1,12 @@
+import React , {Fragment} from 'react'
+import OverviewSection from './OverviewSection';
+
+type Props = {}
+
+export default function index({}: Props) {
+  return (
+    <Fragment>
+        <OverviewSection />
+    </Fragment>
+  )
+}
