275222c438af789f4e64a9660917bf58f6ddcf8a
nevine1 <venasamaan@gmail.com>
2022-10-02 22:13:59 -0400

    new components Styled

--- components/ourPrograms/Tabs.tsx
+++ components/ourPrograms/Tabs.tsx
@@ -1,15 +1,29 @@
 import React, {Fragment} from 'react'
-
+import styled from 'styled-components';
 type Props = {}
 
+const TabsWrapper = styled.div`
+  display: flex; 
+  gap: 30px;
+  background-color: pink;
+  margin: 0;
+
+`;
+const TabSapn = styled.span`
+  padding: 20px 10px;
+  font-weight: 400; 
+  font-size: 16px;
+  color: #6b6969;
+`;
 export default function Tabs({}: Props) {
   return (
     <Fragment>
-        <div>
-            <span>One</span>
-            <span>Tow</span>
-            <span>Three</span>
-        </div>
+        <TabsWrapper>
+            <TabSapn>First Day</TabSapn>
+            <TabSapn>Second Day</TabSapn>
+            <TabSapn>Third Day</TabSapn>
+          
+        </TabsWrapper>
     </Fragment>
   )
 }
